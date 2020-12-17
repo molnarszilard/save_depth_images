18d2b3941e2aeac0d2ae2fe5094fe71065168411
Szilárd Molnár <molnarszilard10@gmail.com>
2020-12-17 10:56:45 +0200

    some mod in depthir

--- src/save_depth_images.cpp
+++ src/save_depth_images.cpp
@@ -67,6 +67,7 @@ void callback(const ImageConstPtr &ir, const ImageConstPtr &depth, const PointCl
     //ROS_INFO_STREAM("Data Arrival\n");
 
     cv_bridge::CvImagePtr img_ptr_ir;
+    cv_bridge::CvImagePtr img_ptr_ir_contrast;
     cv_bridge::CvImagePtr img_ptr_rgb;
     cv_bridge::CvImagePtr img_ptr_depth;
     img_ptr_ir = cv_bridge::toCvCopy(*ir, sensor_msgs::image_encodings::TYPE_16UC1);
@@ -96,14 +97,14 @@ void callback(const ImageConstPtr &ir, const ImageConstPtr &depth, const PointCl
     pcl::io::savePCDFileASCII (file_pcd, *cloud_in);    
     // cv::convertScaleAbs(mat_depth, mat_depth, 0.03, 1.0);
     // cv::Mat zerochannel = cv::Mat::zeros(cv::Size(mat_depth.rows, mat_depth.cols), CV_16U);
-    cv::Mat output = cv::Mat::zeros(mat_depth.rows, mat_depth.cols, CV_8UC3);
+    cv::Mat output = cv::Mat::zeros(mat_depth.rows, mat_depth.cols, CV_16UC3);
     cv::Mat images[3] = {mat_ir, mat_depth, mat_ir_contrast};
     int dims[3] = {2, mat_depth.rows, mat_depth.cols};
     cv::Mat joined(3, dims, CV_8U);
     for (int i = 0; i < 3; ++i)
     {
-        uint8_t *ptr = &joined.at<uint8_t>(i, 0, 0);                              // pointer to first element of slice i
-        cv::Mat destination(mat_depth.rows, mat_depth.cols, CV_8U, (void *)ptr); // no data copy, see documentation
+        uint8_t *ptr = &joined.at<uint16_t>(i, 0, 0);                              // pointer to first element of slice i
+        cv::Mat destination(mat_depth.rows, mat_depth.cols, CV_16U, (void *)ptr); // no data copy, see documentation
         images[i].copyTo(destination);
     }
 
@@ -113,6 +114,7 @@ void callback(const ImageConstPtr &ir, const ImageConstPtr &depth, const PointCl
         {
             output.at<cv::Vec3b>(x, y)[2] = images[0].at<uchar>(x, y);
             output.at<cv::Vec3b>(x, y)[1] = images[1].at<uchar>(x, y);
+            output.at<cv::Vec3b>(x, y)[0] = images[2].at<uchar>(x, y);
         }
     }
     

f48c81957f4c3eb7fce1c1fad3d9b9803bf38607
Szilárd Molnár <molnarszilard10@gmail.com>
2020-12-17 11:05:04 +0200

    enc mod2

--- src/save_depth_images.cpp
+++ src/save_depth_images.cpp
@@ -103,7 +103,7 @@ void callback(const ImageConstPtr &ir, const ImageConstPtr &depth, const PointCl
     cv::Mat joined(3, dims, CV_16U);
     for (int i = 0; i < 3; ++i)
     {
-        uint8_t *ptr = &joined.at<uint16_t>(i, 0, 0);                              // pointer to first element of slice i
+        uint8_t *ptr = &joined.at<uint8_t>(i, 0, 0);                              // pointer to first element of slice i
         cv::Mat destination(mat_depth.rows, mat_depth.cols, CV_16U, (void *)ptr); // no data copy, see documentation
         images[i].copyTo(destination);
     }
